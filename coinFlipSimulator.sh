0ee980c5dbc90b4f8cacb19fdd3ff478ac45c1e6
mebangit123 <mebannongrum333@gmail.com>
2021-03-29 17:19:39 +0530

    Added coinflip2 to main branch

--- coinFlipSimulator.sh
+++ coinFlipSimulator.sh
@@ -1,10 +1,21 @@
 #!/bin/bash -x
 echo "Flip Coin Simulator"
 
-FLIP=$(($(($RANDOM%10))%2))
-if [ $FLIP -eq 1 ]
+heads=0
+tails=0
+
+while (( $heads < 10 && $tails < 10 ))
+do
+
+FLIP=$(($RANDOM%2))
+
+if [ $FLIP == 1 ]
 then
-    echo "heads"
+    heads=$(($heads+1))
 else
-    echo "tails"
+    tails=$(($tails+1))
 fi
+done
+
+echo "Heads win $heads times."
+echo "Tails win $tails times."
